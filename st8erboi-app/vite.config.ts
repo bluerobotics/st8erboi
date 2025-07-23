75530254c76bdd7a99f98a029b93096af9b30fb8
Eldin Miller-Stead <eldin@bluerobotics.com>
2025-07-23 16:21:00 -0700

    1

--- st8erboi-app/vite.config.ts
+++ st8erboi-app/vite.config.ts
@@ -1,26 +1,19 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import electron from 'vite-plugin-electron/simple';
+import path from 'node:path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // The css block is no longer needed
   plugins: [
     vue(),
     electron({
       main: {
-        // Entry point for the Electron main process
-        entry: 'electron/main.ts',
+        entry: path.join(__dirname, 'electron/main.ts'),
       },
       preload: {
-        // Entry point for the Electron preload script
         input: path.join(__dirname, 'electron/preload.ts'),
       },
-      // Enables Node.js API in the renderer process
-      renderer: process.env.NODE_ENV === 'test'
-        ? undefined
-        : {},
+      renderer: {},
     }),
   ],
-})
+});
